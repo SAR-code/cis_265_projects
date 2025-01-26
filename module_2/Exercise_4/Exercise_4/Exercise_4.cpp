87a8008343fffd61509377af04dbcabb9c9eddd0
SAR-code <dylanmccallum28@gmail.com>
2025-01-26 16:21:45 -0700

    part one completed

---
+++ module_2/Exercise_4/Exercise_4/Exercise_4.cpp
@@ -0,0 +1,18 @@
+/*
+* SCRIPT: Exercise_4.c
+* PURPOSE: Translates an alphabetic phone number into numeric form
+* AUTHOR: D.M
+* DATE: 26JAN25
+*
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int main()
+{
+    printf("Hello World!\n");
+}
+
+
